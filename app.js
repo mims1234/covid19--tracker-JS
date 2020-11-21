0112d888de493dbbeec98765d5904e244fddc46c
MiMs <imranmohsin184@gmail.com>
2020-11-21 11:41:12 +0530

    modification

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
 require("dotenv").config()
+const port = process.env.PORT; 
+
+app.set("port",port)
 
 app.get("/", (req,res) =>{
     res.send("Test Page");
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running at http://localhost:${process.env.PORT}/`);
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 })
